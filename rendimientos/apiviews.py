bdf73d91a13dfc03c2c6e9f47b3a42f3bf41ee5f
Luciano Hidalgo <whitchking@gmail.com>
2018-08-06 15:24:11 -0400

    Se agrega el filtro al viewset de rendimientos, se cambian los modelos a readOnly

--- rendimientos/apiviews.py
+++ rendimientos/apiviews.py
@@ -2,24 +2,36 @@ from django.shortcuts import render_to_response
 import rendimientos.serializers.carreraSerializer as carSerializers
 from .models import Carrera, TipoCarrera, RendimientoCarrera
 from rest_framework import viewsets, generics
-from django.template import RequestContext
 
+# TESTING
+from django.template import RequestContext
 import django_filters.rest_framework
 
 
-class CarreraViewSet(viewsets.ModelViewSet):
+
+class CarreraViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Carrera.objects.all()
     serializer_class = carSerializers.CarreraSerializer
     # Para manejar permisos se explica en https://youtu.be/VZ8NIoLN-yQ
 
 
 
-class RendimientoCarreraViewSet(viewsets.ModelViewSet):
-    queryset = RendimientoCarrera.objects.all()
+class RendimientoCarreraViewSet(viewsets.ReadOnlyModelViewSet):
+
     serializer_class = carSerializers.RendimientoCarreraSerializer
 
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = RendimientoCarrera.objects.all()
+        codigo_carrera = self.request.query_params.get('carr', None)
+        codigo_asignatura = self.request.query_params.get('asig', None)
+        if codigo_carrera != None :
+            queryset_list = queryset_list.filter(codigo_carrera=codigo_carrera)
+        if codigo_asignatura != None :
+            queryset_list = queryset_list.filter(codigo_asignatura=codigo_asignatura)
+        return queryset_list
 
-class TipoCarreraViewSet(viewsets.ModelViewSet):
+
+class TipoCarreraViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = TipoCarrera.objects.all()
     serializer_class = carSerializers.TipoCarreraSerializer
 
--- rendimientos/urls.py
+++ rendimientos/urls.py
@@ -1,18 +1,19 @@
-from django.urls import path, include
+from django.urls import re_path, include, path
 from . import views
 
 from . import apiviews
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('carrera',apiviews.CarreraViewSet)
-router.register('rendimiento_carrera', apiviews.RendimientoCarreraViewSet)
-router.register('tipo_carrera', apiviews.TipoCarreraViewSet)
+router.register(r'carrera',apiviews.CarreraViewSet)
+#router.register(r'^rendimiento_carrera/(?P<cod_asignatura>\d+)/(?P<cod_carrera>\d+)/$', apiviews.RendimientoCarreraViewSet.as_view())
+router.register(r'rendimiento_carrera', apiviews.RendimientoCarreraViewSet, base_name='rendimiento_carrera')
+router.register(r'tipo_carrera', apiviews.TipoCarreraViewSet)
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:codigo>/', views.rendimientoCarrera, {'soloregulares': True}, name='detalle'),
     path('<int:codigo>/1', views.rendimientoCarrera , {'soloregulares': False},name='detallefiltrado'),
-    path('api/', include(router.urls)),
+   re_path(r'^api/', include(router.urls)),
 ]
